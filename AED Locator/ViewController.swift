8c9fcfcd19d46796aebbe3593ea1a06d1f96cfae
Sean Hickey <shickey@blakeschool.org>
2016-03-05 19:37:14 -0600

    Initial Commit

---
+++ AED Locator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AED Locator
+//
+//  Created by Sean Hickey on 3/5/16.
+//  Copyright © 2016 GeoCode2016. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
